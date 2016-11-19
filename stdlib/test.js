0ec04846f638e2e429204a91802baa7bd3d2e031
Zhipeng <jsyxzzp@outlook.com>
2016-11-19 14:42:47 -0500

    Play with your .me

--- stdlib/test.js
+++ stdlib/test.js
@@ -5,11 +5,22 @@ const ast1 = require('../libpepper/ast.1');
 const boot1 = require('../libpepper/boot.1');
 const boot2 = require('../libpepper/boot.2.js')
 
-// var a = 'hello'
-const codeAst1 = ast1.call(ast1.lookup('__assign'), [
-    ast1.symbol('a', 'var'),
-    ast1.literal('hello', 'string')
-])
+// write
+const codeAst1 = ast1.call(ast1.lookup('read'),[
+    ast1.call(ast1.lookup('__assign'), [
+        ast1.symbol('a', 'var'),
+        ast1.literal('txt', 'string'),
+    ]),
+    ast1.call(ast1.lookup('__assign'),[
+        ast1.symbol('b', 'const'),
+        ast1.code(
+            ['x'], ['const'], '',
+            ast1.call(asta.lookup('__assign'),[
+                ast1.lookup('a'),
+                ast1.lookup('x'),
+            ]))
+    ])
+]);
 
 const passBoot1 = boot1();
 const passBoot2 = boot2();
