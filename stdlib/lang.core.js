1e0a525a922e058ca75131f50df10f229329c9f6
wally yang <wallyyang28@gmail.com>
2016-11-19 12:54:19 -0500

    Add write function

--- stdlib/lang.core.js
+++ stdlib/lang.core.js
@@ -5,30 +5,52 @@ const ast2 = require('../libpepper/ast.2')
 const typeInfo = require('../libpepper/type.info')
 
 module.exports = (boot) => {
-    //　assignment
+    //　__assign('target', 'source')
     boot.namedModule(
         '__assign', 'const', ast1.code(
-            ['target', 'source'], ['out', 'const'], '',
-                ast1.meta(
-                    (pass, instance) => {
-                        instance.accessIn(
-                            'target',
-                            instance.accessOut('source')
-                        );
+            ['target', 'source'], ['out', 'const'], '', ast1.meta(
+                (pass, instance) => {
+                    instance.accessIn(
+                        'target',
+                        instance.accessOut('source')
+                    );
 
-                        return ast2.nativeOut(
-                            {
-                                js: (pass, target) => {
-                                    pass.write('__self.set(\'target\', __self.get(\'source\'))');
-                                }
-                            },
-                            typeInfo.basic('null')
-                        );
-                    },
-                    (pass, instance, type) => {
-                        throw Error();
-                    }
-                )
+                    return ast2.nativeOut(
+                        {
+                            js: (pass, target) => {
+                                pass.write('__self.set(\'target\', __self.get(\'source\'))');
+                              }
+                        },
+                        typeInfo.basic('null')
+                    );
+                },
+                (pass, instance, type) => {
+                    throw Error();
+                }
             )
+        )
     );
+
+    //  write('val')
+    boot.namedModule(
+        'write', 'const', ast1.code(
+            ['val'], ['const'], '', ast1.meta(
+                (pass, instance) => {
+                    return ast2.nativeOut(
+                        {
+                            js: (pass, target) => {
+                                pass.write('console.log(__self.get(\'val\'))');
+                            }
+                        },
+                        typeInfo.basic('null')
+                    );
+                },
+                (pass, instance, type) => {
+                    throw Error();
+                }
+            )
+        )
+    );
+
+    // 
 };
